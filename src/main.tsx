9010b11b568a4f373e384deefa4842bff4ba88f4
Cetin Omer, AVL/TR <omer.cetin@avl.com>
2024-12-23 17:20:00 +0300

    fix(): main

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
 
-Amplify.configure(outputs);
-
+// Provide the required props to App
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <App 
+      pageProps={{}} 
+      Component={() => <div>Welcome!</div>} 
+      router={{} as any} // Provide a mock router object
+    />
   </React.StrictMode>
 );
